d928a4108b8676547b2297a66e92dad4de7476ed
Matt Bow <mattybow@gmail.com>
2015-06-12 15:48:00 -0500

    reset login

--- server/logins.js
+++ server/logins.js
@@ -3,6 +3,10 @@ isValidUser = function(username){
 	return WHITE_LIST.indexOf(username) >= 0;
 };
 Accounts.validateLoginAttempt(function(user){
-	var username = user.user.services.github.username;
-	return isValidUser(username);
+	console.log(user);
+	if(user.user){
+		var username = user.user.services.github.username;
+		return isValidUser(username);
+	}
+	return false;
 });
