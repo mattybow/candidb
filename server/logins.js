a43969e4c686dcffd40254b5f34f99cd316a5b60
Matt Bow <mattybow@gmail.com>
2015-06-12 19:34:49 -0500

    twitter login tweek

--- server/logins.js
+++ server/logins.js
@@ -1,9 +1,9 @@
-WHITE_LIST = ['Dmthigh','bmagnantb'];
+WHITE_LIST = ['DmtHigh','mattybow','bmagnantb'];
 isValidUser = function(username){
 	return WHITE_LIST.indexOf(username) >= 0;
 };
 Accounts.validateLoginAttempt(function(user){
-	console.log(user);
+	AppErrors.insert({data:JSON.stringify(user));
 	if(user.user){
 		var username = user.user.services.twitter.username;
 		return isValidUser(username);
